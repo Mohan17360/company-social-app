de8e1bfb4999007f37e3ab8dbdcea5a3aa8d7058
Mohan17360 <shreemene25@gmail.com>
2026-06-06 10:31:01 +0530

    Added Firestore feed display

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -1,20 +1,44 @@
-import { useState } from "react";
-import { addDoc, collection } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import {
+  addDoc,
+  collection,
+  getDocs
+} from "firebase/firestore";
 import { db } from "../firebase";
 
 function FeedPage() {
   const [post, setPost] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  const loadPosts = async () => {
+    const querySnapshot = await getDocs(
+      collection(db, "posts")
+    );
+
+    const postList = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    setPosts(postList.reverse());
+  };
+
+  useEffect(() => {
+    loadPosts();
+  }, []);
 
   const handlePost = async () => {
     try {
       await addDoc(collection(db, "posts"), {
         content: post,
-        createdAt: new Date()
+        createdAt: new Date(),
       });
 
-      alert("Post Created!");
-
       setPost("");
+
+      loadPosts();
+
+      alert("Post Created!");
     } catch (error) {
       alert(error.message);
     }
@@ -32,11 +56,29 @@ function FeedPage() {
         onChange={(e) => setPost(e.target.value)}
       />
 
-      <br /><br />
+      <br />
+      <br />
 
       <button onClick={handlePost}>
         Create Post
       </button>
+
+      <hr />
+
+      <h2>Posts</h2>
+
+      {posts.map((item) => (
+        <div
+          key={item.id}
+          style={{
+            border: "1px solid gray",
+            padding: "10px",
+            marginBottom: "10px",
+          }}
+        >
+          <p>{item.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
